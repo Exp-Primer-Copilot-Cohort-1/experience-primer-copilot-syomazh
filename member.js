034b41767a6ac67ea15c952c5c149582e6a79398
syomazh <108838306+syomazh@users.noreply.github.com>
2024-05-28 03:35:50 -0500

    vdgdgbdgb

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  const memberSkills = {
+    name: 'John',
+    age: 23,
+    skills: ['Javascript', 'React', 'Node', 'MongoDB', 'HTML', 'CSS', 'SCSS', 'Python', 'Django', 'Flask']
+  };
+
+  return memberSkills;
+}
